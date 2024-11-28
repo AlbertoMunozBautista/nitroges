24559382644d65b8980f56a24bb7c9223d4d5481
Alberto Muñoz Bautista <albertomunozbautista@gmail.com>
2024-11-28 19:47:27 +0100

    se crean las clases estadoingreso y tipoingreso

---
+++ src/main/java/com/ivangas/nitrogest/model/TipoIngreso.java
@@ -0,0 +1,23 @@
+package com.ivangas.nitrogest.model;
+
+public class TipoIngreso {
+
+    private Long id;
+    private String tipoIngreso;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTipoIngreso() {
+        return tipoIngreso;
+    }
+
+    public void setTipoIngreso(String tipoIngreso) {
+        this.tipoIngreso = tipoIngreso;
+    }
+}
